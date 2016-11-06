68eb6c515f32eed609ce280d540a11f72418e713
Harley Trung <harley@bsa.edu.vn>
2016-11-06 18:28:04 +0700

    minor

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,17 +14,18 @@ class MessagesController < ApplicationController
     load_room
     @message = @room.messages.build message_params
     @message.username = current_user
+    @message.save
 
     respond_to do |format|
       format.html do
-        if @message.save
+        if @message.persisted?
           redirect_to room_messages_path(@room)
         else
           flash[:error] = "Error: #{@message.errors.full_messages.to_sentence}"
           redirect_to root_path
         end
       end
-      format.js 
+      format.js
     end
   end
 
